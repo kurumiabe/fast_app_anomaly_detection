bc37a0c98ec76571cd810c2de98094155378ec32
kurumiabe <58407447+kurumiabe@users.noreply.github.com>
2024-04-28 16:03:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -66,4 +66,5 @@ async def upload_zip(file: UploadFile = File(...)):
         return {"results": results}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # 環境変数PORTを取得、デフォルトは8000
+    uvicorn.run(app, host="0.0.0.0", port=port)
